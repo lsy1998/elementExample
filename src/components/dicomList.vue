f8109202f9ee555532ca21597b12bca6c2a38a8b
lsy1998 <1945934866@qq.com>
2020-04-12 00:46:35 +0800

    dicom列表优化

--- src/components/dicomList.vue
+++ src/components/dicomList.vue
@@ -1,104 +1,64 @@
 
 <template>
   <div id="dicomListDiv" style="">
-      <el-row style="padding:100px 0 0 0">
-          <el-col style="color:white;" :span='4'>brave</el-col>
-          <el-col :span='16'>
-              <el-table
-    :data="tableData"
-    height="250"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-          </el-col>
-          <el-col style="color:white;" :span='4'>confidence</el-col>
-      </el-row>
-      <div style="width:100%">
+    <el-row style="padding:100px 0 0 0">
+      <el-col style="color:white;" :span='4'>brave</el-col>
+      <el-col :span='16'>
+        <el-table :data="tableData"  :border=true style="width: 100%" fit highlight-current-row max-height="750">
+          <el-table-column prop="dicomId" label="序号" align="center">
+          </el-table-column>
+          <el-table-column prop="dicomPath" label="DICOM" align="center">
+          </el-table-column>
+           <el-table-column prop="dicomLength" label="图像数" align="center">
+          </el-table-column>
+          <el-table-column label="操作" align="center">
+            <template slot-scope="scope">
+              <el-button type="primary" size="mini" @click="handleEdit(scope.$index, scope.row)" >查看</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div style="display:inline-block;float:left;">数据源：</div><div style="display:inline-block;float:left; margin:4px 0 0 0;"><a href="https://wiki.cancerimagingarchive.net/display/Public/LIDC-IDRI" style="text-decoration:none;">LIDC-IDRI</a></div>
+      </el-col>
+      <el-col style="color:white;" :span='4'>confidence</el-col>
+    </el-row>
+    <div style="width:100%">
 
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import $ from 'jquery'
+import axios from 'axios'
 export default {
   name: 'dicomList',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      tableData: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }]
+      tableData: []
     }
   },
   methods: {
     handleEdit (index, row) {
       console.log(index, row)
-    },
-    handleDelete (index, row) {
-      console.log(index, row)
+      // console.log()
+      // alert(row.dicomId)
+      // alert(row.dicomPath)
+      // alert(row.dicomLength)
     }
   },
   mounted () {
     $('#dicomListDiv').width($(window).width())
     $('#dicomListDiv').height($(window).height())
+    axios.get('http://localhost:8082/getDicomList')
+      .then((response) => {
+        console.log(response.data)
+        this.tableData = response.data.dicomList
+      })
   }
 }
 </script>
 
 <style scoped>
-
-@import '../assets/iconfont/iconfont.css'
-
+@import "../assets/iconfont/iconfont.css";
 </style>
